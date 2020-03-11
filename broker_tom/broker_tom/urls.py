39f2d8b6a13ab5771e4b1afca035a44a0b99a9e1
Daniel Perrefort <djperrefort@pitt.edu>
2020-03-11 17:04:21 -0400

    Adds a website section for individual objects

--- broker_tom/broker_tom/urls.py
+++ broker_tom/broker_tom/urls.py
@@ -8,13 +8,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from .views import AlertSummaryView, AlertsView, IndexView
+from .views import AlertSummaryView, AlertsView, IndexView, ObjectSummaryView, ObjectsView
 
 # URLs for custom views
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('alerts/', AlertsView.as_view(), name='alerts'),
     path('alerts/<int:pk>', AlertSummaryView.as_view(), name='alert-summary'),
+    path('objects/', ObjectsView.as_view(), name='objects'),
+    path('objects/<int:pk>', ObjectSummaryView.as_view(), name='object-summary'),
     path('gettingstarted/', include('getting_started.urls')),
     path('user/', include('user_subscriptions.urls')),
 ]
