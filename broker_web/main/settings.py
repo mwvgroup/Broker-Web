485eca723c76a94648279ca75a51c2e5f73b4552
djperrefort <djperrefort@pitt.edu>
2020-04-19 00:20:35 -0400

    Switches offline backend back to mysql

--- broker_web/main/settings.py
+++ broker_web/main/settings.py
@@ -128,8 +128,12 @@ else:
     # > exit
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'brokerweb',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
         }
     }
 
