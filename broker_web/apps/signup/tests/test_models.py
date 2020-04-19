706d55f87e364645f6b7482da2078d062a3a3722
djperrefort <djperrefort@pitt.edu>
2020-04-18 22:25:23 -0400

    Minor organizational edits

--- broker_web/apps/signup/tests/test_models.py
+++ broker_web/apps/signup/tests/test_models.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-"""Tests for the ``managers`` module"""
+"""Tests for the ``models`` and ``managers`` modules"""
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
@@ -13,10 +13,10 @@ class CustomUserManager(TestCase):
     def test_create_user(self):
         """Test creation of regular user has correct defaults"""
 
-        UserModel = get_user_model()
+        user_model = get_user_model()
 
         test_email = 'test@user.com'
-        new_user = UserModel.objects.create_user(email=test_email, password='test')
+        new_user = user_model.objects.create_user(email=test_email, password='test')
 
         self.assertEqual(new_user.email, test_email, 'Email does not equal passed value.')
         self.assertFalse(new_user.is_active, 'User is set to active')
@@ -24,18 +24,18 @@ class CustomUserManager(TestCase):
         self.assertFalse(new_user.is_superuser, 'User is superuser')
 
         with self.assertRaises(TypeError, msg='No error raised for missing email'):
-            UserModel.objects.create_user()
+            user_model.objects.create_user()
 
         with self.assertRaises(TypeError, msg='No error raised for blank email'):
-            UserModel.objects.create_user(email='')
+            user_model.objects.create_user(email='')
 
     def test_create_superuser(self):
         """Test creation of a superuser has correct defaults"""
 
-        UserModel = get_user_model()
+        user_model = get_user_model()
 
         test_email = 'test@user.com'
-        new_admin_user = UserModel.objects.create_superuser(test_email, 'foo')
+        new_admin_user = user_model.objects.create_superuser(test_email, 'foo')
 
         self.assertEqual(new_admin_user.email, test_email, 'Email does not equal passed value.')
         self.assertTrue(new_admin_user.is_active, 'User is not active')
@@ -43,11 +43,11 @@ class CustomUserManager(TestCase):
         self.assertTrue(new_admin_user.is_superuser, 'User is not superuser')
 
         with self.assertRaises(TypeError, msg='No error raised for missing email'):
-            UserModel.objects.create_superuser()
+            user_model.objects.create_superuser()
 
         with self.assertRaises(TypeError, msg='No error raised for blank email'):
-            UserModel.objects.create_superuser(email='')
+            user_model.objects.create_superuser(email='')
 
         with self.assertRaises(ValueError, msg='No error raised for is_superuser=False'):
-            UserModel.objects.create_superuser(
+            user_model.objects.create_superuser(
                 email='super@user.com', password='foo', is_superuser=False)
