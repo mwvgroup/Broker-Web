d116a47c175e5f63bd0ab0e231ae7319dfbd09e5
djperrefort <djperrefort@pitt.edu>
2020-04-20 16:13:25 -0400

    Switches to third party recaptcha

--- broker_web/apps/signup/forms.py
+++ broker_web/apps/signup/forms.py
@@ -5,6 +5,7 @@
 construction.
 """
 
+from captcha.fields import ReCaptchaField
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import CustomUser
@@ -15,9 +16,16 @@ user_fields = 'email', 'first_name', 'last_name', 'country', 'university'
 class CustomUserCreationForm(UserCreationForm):
     """Custom form for creating a new ``CustomUser``"""
 
+    captcha = ReCaptchaField()
+
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = user_fields
+
+    def validate(self, value):
+        """Check if value consists only of valid emails."""
+        print(value)
+        super().validate(value)
 
 
 class CustomUserChangeForm(UserChangeForm):
